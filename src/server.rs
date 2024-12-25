094e65002fd3bb8842989e60f005b2d71af3c6ab
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2024-12-26 00:07:20 +0400

    comments

--- src/server.rs
+++ src/server.rs
@@ -6,11 +6,17 @@ pub struct AppState {
     app_name: String,
 }
 
+/// Index route
+/// 
+/// This route is used as a basic health check.
 #[get("/")]
 async fn index(data: web::Data<AppState>) -> String {
     format!("Hello {} from {:?}!", data.app_name, CONFIG.host)
 }
 
+/// Initialize the server
+/// 
+/// This function configures the server with the necessary middleware and routes.
 pub fn initialize(cfg: &mut web::ServiceConfig) {
     cfg.app_data(PathConfig::default().error_handler(handle_error));
     cfg.app_data(QueryConfig::default().error_handler(handle_error));
